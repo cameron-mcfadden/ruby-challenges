07d6a36bcf8ca0cdd2802b4a444484fc7e0468d7
Cameron <Cameron@TheRidgeCCs-MacBook-Pro.local>
2017-09-30 12:18:49 -0500

    My first commit yaaaay

---
+++ ruby_challenges/first_script.rb
@@ -0,0 +1,3 @@
+ruby first_script.rb
+cat > first_script.rb
+puts "Heck yeah this is my first Ruby script!"
---
+++ sites/ruby/class.rb
@@ -0,0 +1,20 @@
+weather = 'rain'
+
+case weather
+when 'rain'
+puts "Use an umbrella!"
+when 'snow'
+	puts "wear snow pants, snow boots, winter jacket, gloves, hat."
+when 'sunny'
+	puts "wear sunscreen, shorts, and short sleeve shirt"
+when 'cloudy' "wear sunscreen, have an umbrella along"
+when 'hot'
+puts "wear shorts, short sleeve shirt, drink a lot of water!"
+when 'cold'
+	puts "wear paints, and sweater"
+when 'warm' 
+	puts "wear shorts, short sleeve shirt"
+when 'really cold'
+	puts "wear 2 pairs of pants, a shirt, 2 sweaters, hat, gloves"
+else
+	puts "Hmm, well I don't know what that weather is!"
